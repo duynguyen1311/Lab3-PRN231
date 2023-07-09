94c0e8ac8c4ee5f690298d239c8c884613a4fc44
duynguyen <duy0256@gmail.com>
2023-07-09 23:06:59 +0700

    init

---
+++ Lab3_PRN231/IdentityAjaxClient/Controllers/ProductController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityAjaxClient.Controllers
+{
+    [Authorize]
+    public class ProductController : Controller
+    {
+        public IActionResult ProductList()
+        {
+            return View();
+        }
+    }
+}
